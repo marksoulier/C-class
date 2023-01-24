3253733018ce846bea7874e4e1174f8f3c8f5d8b
marksoulkid@gmail.com <marksoulkid@gmail.com>
2023-01-24 19:32:16 +0000

    program1 finish

--- program1/program1.cpp
+++ program1/program1.cpp
@@ -1,10 +1,9 @@
 /*******************************************************************
  * @name main
- * @brief Description
- * @param none
- * @retval none
+ * @brief Calculate the square root of 3 coefficents using the quadratic formula
+ * @param a input file containing 3 coefficents a, b, and c on seperate lines
+ * @retval output to file if specified or to terminal if not specified
 *******************************************************************/
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -12,13 +11,16 @@
 #include <iomanip>
 using namespace std;
 
-void quadraticFormula(float a, float b, float c, float& x1, float& x2, int& is_complex);
+void quadraticFormula(float a, float b, float c, float& x1, float& x2, int& is_complex, int& is_zero);
 
 int main(int argc, char *argv[]){
+//declare variables
     float a, b, c;
     float x1, x2;
-    int is_complex;
+    int is_complex, is_zero;
     ifstream file_in;
+
+// open input file
     file_in.open(argv[1], ios::in);
     if (!(file_in.good()))
     {
@@ -26,6 +28,7 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
+//open output file
     ofstream file_out;
     if (argc != 2){
         file_out.open(argv[2], ios::out);
@@ -35,33 +38,46 @@ int main(int argc, char *argv[]){
             return 0;
         }
     }
+// take in first elements
+    file_in >> a;
+    file_in >> b;
+    file_in >> c;
 
+//loop through file printing to output and reading from in
     while(!(file_in.eof())){
         is_complex = 0;
+        is_zero = 0;
+        
+// get roots and errors if problem
+        quadraticFormula(a, b, c, x1, x2, is_complex, is_zero);
 
+//write to file or to terminal
+        if (argc > 2){
+            if ((is_complex == 1)){
+                file_out << a << "\t" << b << "\t" << c << "\t" << "complex" << endl;
+            }else if(is_zero == 1){
+                file_out << a << "\t" << b << "\t" << c << "\t" << "DNE" << endl;
+            }
+            else{
+                file_out << a << "\t" << b << "\t" << c << "\t";
+                file_out << fixed << setprecision(4) << x1 << "\t" << x2 << fixed << setprecision(0) << "\t" << endl;
+            }
+        }else{
+            if (is_complex == 1){
+                cout << a << "\t" << b << "\t" << c << "\t" << "complex" << endl;
+            }else if(is_zero == 1){
+                cout << a << "\t" << b << "\t" << c << "\t" << "DNE" << endl;
+            }
+            else{
+                cout << a << "\t" << b << "\t" << c << "\t";
+                cout << fixed << setprecision(4) << x1 << "\t" << x2 << fixed << setprecision(0) << "\t" << endl;
+            }
+        }
+    
+// read file from a, b, c
         file_in >> a;
         file_in >> b;
         file_in >> c;
-        // read file a, b, c
-
-        quadraticFormula(a, b, c, x1, x2, is_complex);
-
-        //write to file or to terminal
-        if (argc > 2){
-            if ((is_complex == 0)){
-                file_out << a << "\t" << b << "\t" << c << "\t";
-                file_out << fixed << setprecision(4) << x1 << "\t" << x2 << fixed << setprecision(0) << "\t" << endl;
-            }else{
-                file_out << a << "\t" << b << "\t" << c << "\t" << "complex" << endl;
-            }
-        }else{
-            if ((is_complex == 0)){
-                cout << a << "\t" << b << "\t" << c << "\t";
-                cout << fixed << setprecision(4) << x1 << "\t" << x2 << fixed << setprecision(0) <<  "\t" << endl;
-            }else{
-                cout << a << "\t" << b << "\t" << c << "\t" << "complex" << endl;
-            }
-        }
     }
     return 0;
 }
@@ -70,21 +86,31 @@ int main(int argc, char *argv[]){
 /*******************************************************************
  * @name quadraticFormula
  * @brief takes in 3 coefficents and 2 x variables to calculate the quadratic squares
- * @param a, b, c, x1, x2
- * @retval a changed x1, x2
+ * @param a, b, c, x1, x2, is_complex, is_zero
+ * @retval changed x1, x2
 *******************************************************************/
 
-void quadraticFormula(float a, float b, float c, float& x1, float& x2, int& is_complex)
+void quadraticFormula(float a, float b, float c, float& x1, float& x2, int& is_complex, int& is_zero)
 {
     float d;
     float first_part, second_part;
+
+    //calculate the determinate
     d = b*b - 4*a*c;
+
+    // give error for divide by 0
+    if (a == 0){
+        is_zero = 1;
+        return;
+    }
+
+    //if not complex calculate roots
     if (d >= 0){
         first_part = -b / (2 * a);
         second_part = sqrt(d)/(2 * a);
         x1 = first_part + second_part;
         x2 = first_part - second_part;
-    }
+    }   
     else{
         is_complex = 1;
     }
